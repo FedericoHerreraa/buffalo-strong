5eceef08c362dcf04eb018e04df2a23d20c7b99e
CamilaAznar <aznarcami@gmail.com>
2025-02-08 14:56:34 -0300

    adding header

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import { CarouselHomePage } from "@/app/components/CarouselHomePage";
 import { Footer } from "@/app/components/Footer";
 
 
+import Header from "./components/Header"
 
 export default function Home() {
   return (
     <>
+      <Header/>
       <CarouselHomePage />
       <Footer />
     </>
