fcf3e9e64a551af99f3442f03475cca511259919
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-08 15:07:56 -0300

    adding some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { CarouselHomePage } from "@/app/components/CarouselHomePage";
 import { Footer } from "@/app/components/Footer";
-
-
 import Header from "./components/Header"
 
 export default function Home() {
