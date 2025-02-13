bed22f334bb64efe215deaa34a1b17769b97d1e1
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-13 11:55:06 -0300

    email confirmation

--- app/components/LoginController.tsx
+++ app/components/LoginController.tsx
@@ -8,12 +8,12 @@ export const LoginController = () => {
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
     const [loading, setLoading] = useState(false)
-    const { login } = useAuth()
+    const { register } = useAuth()
     
     const loginUser = async () => {
         setLoading(true)
 
-        await login(email, password)
+        await register(email, password, 112233, 'ramon falcon')
 
         setLoading(false)
     }
