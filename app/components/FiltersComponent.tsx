6cf93c0b20c46b265ae357e6c899fcb0ca08c2af
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-14 16:33:42 -0300

    changing width from filters

--- app/components/FiltersComponent.tsx
+++ app/components/FiltersComponent.tsx
@@ -3,7 +3,7 @@ import { merriweather_sans } from "@/app/fonts/fonts"
 
 export const FIltersComponent = () => {
     return (
-        <div className={`border border-zinc-300 rounded-xl w-[95%] mx-auto h-32 bg-zinc-100 p-10 ${merriweather_sans.className}`}>
+        <div className={`border border-zinc-300 rounded-xl w-[90%] mx-auto h-32 bg-zinc-100 p-10 ${merriweather_sans.className}`}>
             <h1>En esta seccion irian los filtros</h1>
         </div>
     )
