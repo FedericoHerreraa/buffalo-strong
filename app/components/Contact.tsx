927d77b16d5ac01d4ed1bfa41bdf962d6dcc1a09
CamilaAznar <aznarcami@gmail.com>
2025-02-16 15:00:58 -0300

    contacto

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,11 +1,11 @@
+import { ContactUsContent } from "./ContactUsContent"
 
 
 
 export const Contact = () => {
     return (
-        <div className="flex flex-col h-[100vh] items-center mt-10">
-            <h1 className="text-4xl mb-3">Contactanos Ahora Mismo.</h1>
-            <p className="text-zinc-600">Seccion para contactar a Buffalo Music.</p>
+        <div className="flex flex-col h-[100vh] items-center ">
+            <ContactUsContent/> 
         </div>
     )
 }
