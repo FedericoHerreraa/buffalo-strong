2701a9c3f3c9942045a936d2429c224bb2126ee1
CamilaAznar <aznarcami@gmail.com>
2025-02-20 14:28:17 -0300

    changes on filters

--- app/components/FiltersComponent.tsx
+++ app/components/FiltersComponent.tsx
@@ -23,25 +23,32 @@ export const FIltersComponent = () => {
     };
 
     return (
-        <div className={`w-full mx-auto h-48 flex items-center justify-center bg-gradient-to-b from-white via-zinc-50 to-white ${merriweather_sans.className}`}>
-            <Carousel className="md:w-[70%] w-fit md:mx-auto" opts={{
-                loop: true
-            }}>
-                <CarouselContent className=" flex items-center text-center">
-                    {Filters.map((item, index) => (
-                        <CarouselItem key={index} className="md:basis-1/4 basis-1/2 flex justify-center flex-col">
-                            <div className="flex flex-col items-center gap-2 p-2" onClick={()=> scrollToSection(item.key)}>
-                                <div className="md:w-24 w-20 md:h-24 h-20 rounded-full bg-white p-2 overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 cursor-pointer">
-                                    <Image alt={item.name} src={item.img} className="w-full h-full object-cover" />
-                                </div>
-                                <p className="text-sm font-semibold text-gray-700">{item.name}</p>
-                            </div>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>
+        <div
+            className={`w-full mx-auto h-48 md:h-56 flex items-center justify-center bg-gradient-to-b from-white via-zinc-50 to-white ${merriweather_sans.className}`}
+        >
+        <Carousel className="w-full max-w-xs md:max-w-lg mx-auto" opts={{ loop: true}}>
+            <CarouselContent className="flex items-center text-center">
+                {Filters.map((item, index) => (
+                    <CarouselItem
+                        key={index}
+                        className="basis-1/2 md:basis-1/4 flex justify-center flex-col"
+                    >
+                    <div className="flex flex-col items-center gap-2 p-2" onClick={() => scrollToSection(item.key)}>
+                        <div className="w-20 h-20 md:w-24 md:h-24 rounded-full bg-white p-2 overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 cursor-pointer">
+                            <Image
+                                alt={item.name}
+                                src={item.img}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <p className="text-sm md:text-base font-semibold text-gray-700">{item.name}</p>
+                    </div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext  />
+        </Carousel>
         </div>
     )
 }
