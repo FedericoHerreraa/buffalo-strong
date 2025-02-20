c0c9dbb0235f40814382ec2c8718a7a2a46d163a
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-20 16:20:46 -0300

    trying to fix main

--- app/components/WaysOfPayment.tsx
+++ app/components/WaysOfPayment.tsx
@@ -28,17 +28,17 @@ export const WaysOfPayment = () => {
 
             <section className="w-[90%] md:w-[60%] p-6 md:p-10 rounded-lg mx-auto flex flex-col md:flex-row items-center gap-6 md:gap-20 text-zinc-700">
             <div className="flex flex-col items-center text-center">
-                <CiCreditCard1 size={50} className="md:size-60" />
+                <CiCreditCard1 size={50} className="md:size-50" />
                 <h2 className="text-lg md:text-xl font-semibold">Tarjeta de Crédito</h2>
                 <p className="text-sm md:text-base">Realiza tus compras con tarjeta de crédito</p>
             </div>
             <div className="flex flex-col items-center text-center">
-                <BsCash size={50} className="md:size-60" />
+                <BsCash size={50} className="md:size-50" />
                 <h2 className="text-lg md:text-xl font-semibold">Efectivo</h2>
                 <p className="text-sm md:text-base">Realiza tus compras con efectivo</p>
             </div>
             <div className="flex flex-col items-center text-center">
-                <SiMercadopago size={50} className="md:size-60" />
+                <SiMercadopago size={50} className="md:size-50" />
                 <h2 className="text-lg md:text-xl font-semibold">Mercado Pago</h2>
                 <p className="text-sm md:text-base">Realiza tus compras con Mercado Pago</p>
             </div>
