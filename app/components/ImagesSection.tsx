8d597438d5b2a4d59e302b765d29e0af856b8019
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-21 18:28:34 -0300

    taking out scale in images

--- app/components/ImagesSection.tsx
+++ app/components/ImagesSection.tsx
@@ -9,7 +9,7 @@ export const ImagesSection = () => {
         <div className={`bg-gradient-to-b from-white via-zinc-100 to-white py-5 ${merriweather_sans.className}`}>
             <div className="w-[85%] mx-auto">
                 <div className="flex gap-3"> 
-                    <div className="w-1/2 h-[300px] rounded-lg hover:scale-105 transition-all duration-200 shadow-lg border border-zinc-200 overflow-hidden">
+                    <div className="w-1/2 h-[300px] rounded-lg transition-all duration-200 shadow-lg border border-zinc-200 overflow-hidden">
                         <Image
                             src={ekoImage}
                             alt="Imagen 1"
@@ -18,7 +18,7 @@ export const ImagesSection = () => {
                             className="w-full h-full object-cover"
                         />
                     </div>
-                    <div className="w-1/2 h-[300px] rounded-lg hover:scale-105 transition-all duration-200 shadow-lg border border-zinc-200 overflow-hidden">
+                    <div className="w-1/2 h-[300px] rounded-lg transition-all duration-200 shadow-lg border border-zinc-200 overflow-hidden">
                         <Image
                             src={otherImage}
                             alt="Imagen 2"
