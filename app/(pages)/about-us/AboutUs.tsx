0388e4cb23e767ad974a486f118480b8424f8fcb
CamilaAznar <aznarcami@gmail.com>
2025-02-24 12:26:34 -0300

    cambios en nosotros

--- app/(pages)/about-us/AboutUs.tsx
+++ app/(pages)/about-us/AboutUs.tsx
@@ -4,8 +4,8 @@ import { TitleSection } from "@/app/components/reusable/titleSection";
 
 export const AboutUsComponent = () => {
     return (
-        <div className="flex flex-col min-h-[100vh] items-center my-10">
-            <TitleSection title="Sobre Nosotros" description="En esta pagina nos vas a conocer un poco mas"/>
+        <div className="flex flex-col min-h-[100vh] w-full items-center py-6 bg-[#271c04]">
+            <TitleSection title="Sobre Nosotros" description="En esta pagina nos vas a conocer un poco mas" color="from-[#e6b993] via-[#94684a] to-[#6e482d]" colorDescription="text-zinc-300"/>
             <Timeline/>
         </div>
     )
--- app/(pages)/about-us/Timeline.tsx
+++ app/(pages)/about-us/Timeline.tsx
@@ -1,50 +1,39 @@
 import React from "react";
-
+import Image from "next/image";
+import guitarra from "@/app/images/nosotros/guitarras.jpg";
+import instrumento from "@/app/images/nosotros/tocando.jpg";
 
 export const Timeline = () => {
   return (
-    <div className="relative w-full flex flex-col items-center p-8">
-        <div className="absolute left-1/2 transform -translate-x-1/2 w-2 bg-gray-800 h-full"> 
-          <div className=" h-full w-1 rounded-full"></div> 
-        </div>
-        <div className="flex flex-col items-center gap-12 mt-8 w-full">
-          {content.map((item, index) => (
-            <div key={item.id} className={`relative flex ${index % 2 === 0 ? 'flex-row-reverse' : 'flex-row'} items-center w-full max-w-4xl`}>
-              <div className="absolute left-1/2 transform -translate-x-1/2 w-6 h-6 bg-gray-800 rounded-full"></div>
-              <div className={`absolute ${index % 2 === 0 ? 'left-[50%]' : 'right-[50%]'}  w-28 h-1 bg-gray-800`}></div>
-              <div className="bg-white shadow-lg p-4 rounded-lg w-80 border border-gray-300">
-                <p className="text-gray-700 text-center">{item.text}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-};
-  
-  
-const content = [
-    {
-      id: 1,
-      text: "lorem ipsum dolem sajfn dajna fweofew ewoifdknsmnv dlvnsln dxscklnefewf dcxkk cksks sksmskmd "
-    },
-    {
-      id: 2,
-      text: "lorem ipsum dolem sajfn dajna fweofew ewoifdknsmnv dlvnsln dxscklnefewf dcxkk cksks sksmskmd "
-    },
-    {
-      id: 3,
-      text: "lorem ipsum dolem sajfn dajna fweofew ewoifdknsmnv dlvnsln dxscklnefewf dcxkk cksks sksmskmd "
-    },
-    {
-      id: 4,
-      text: "lorem ipsum dolem sajfn dajna fweofew ewoifdknsmnv dlvnsln dxscklnefewf dcxkk cksks sksmskmd "
-    },
-    {
-      id: 5,
-      text: "lorem ipsum dolem sajfn dajna fweofew ewoifdknsmnv dlvnsln dxscklnefewf dcxkk cksks sksmskmd "
-    }
-  
-];
-  
+    <section className="w-full h-screen flex flex-col md:grid md:grid-cols-2">
+      {content.map((item) => (
+        <div key={item.id} className="relative w-full h-full group">
+          {/* Imagen de fondo */}
+          <Image
+            alt="Imagen"
+            src={item.image}
+            className="absolute top-0 left-0 w-full h-full object-cover group-hover:opacity-90 transition-opacity duration-300"
+          />
 
+          {/* Contenedor de texto que aparece al hacer hover */}
+          <div className="absolute inset-0 bg-black bg-opacity-60 text-white p-8 flex items-center justify-center  ">
+            <p className="text-lg md:text-xl text-center">{item.text}</p>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+const content = [
+  {
+    id: 1,
+    text: "Buffalo Strong nació con la pasión por la música y el deseo de acercar instrumentos de alta calidad a artistas de todo el mundo. Desde nuestros inicios, nos especializamos en la importación de instrumentos musicales únicos, seleccionados por su sonido y durabilidad. Trabajamos con marcas reconocidas y luthiers independientes para ofrecer una colección exclusiva a nuestros clientes.",
+    image: guitarra,
+  },
+  {
+    id: 2,
+    text: "Nuestra comunidad de músicos crece día a día, brindando asesoramiento y soporte personalizado a cada cliente. Hoy, Buffalo Strong es sinónimo de calidad, pasión y compromiso con la música, y seguimos ampliando nuestro catálogo para vos.",
+    image: instrumento,
+  },
+];
