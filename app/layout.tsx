20f60881730dbad16d605f85906f658a72935773
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-25 15:18:28 -0300

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ import { MobileViewProvider } from "./context/MobileContext";
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="https://zbixbwkzoftoplldzjya.supabase.co/storage/v1/object/sign/Images/logos/Logobuffalo.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJbWFnZXMvbG9nb3MvTG9nb2J1ZmZhbG8ucG5nIiwiaWF0IjoxNzQwNTA3NDg1LCJleHAiOjE4OTgxODc0ODV9.eATxqb4-ATyweVjeCqZ-p4pfoqF0lKbdXRiJR7Xf_Tw"/>
+      </head>
       <body>
         <CartProvider>
           <AuthProvider>
