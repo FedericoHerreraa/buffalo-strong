f256af3c89b57a872685cb2246d2a12e04866381
Federico Herrera <fede.juan.herrera@gmail.com>
2025-02-27 09:33:31 -0300

    udpating back to top in mobile

--- app/components/BackToTop.tsx
+++ app/components/BackToTop.tsx
@@ -6,7 +6,7 @@ export const BackToTop = () => {
     return (
         <div 
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-            className={`border rounded-lg bg-zinc-800 text-zinc-300 fixed bottom-4 right-4 p-2  cursor-pointer shadow-lg hover:scale-110 transition-transform duration-300 z-50`}
+            className={`border rounded-lg bg-zinc-800 text-zinc-300 fixed bottom-2 right-2 md:p-2 p-1  cursor-pointer shadow-lg hover:scale-110 transition-transform duration-300 z-50`}
         >
             <IoArrowUp size={20}/>
         </div>
