9ea4d723896dc02f74f14d965559da2767b4a277
Federico Herrera <fede.juan.herrera@gmail.com>
2025-03-10 16:11:50 -0300

    improving view from products

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -15,7 +15,7 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
       key={index}
       className="md:min-w-[250px] md:max-w-[250px] min-w-[180px] max-w-[180px] md:min-h-[450px] min-h-[200px] border border-zinc-200 bg-white rounded-md md:hover:shadow-lg transition-all duration-200 cursor-pointer"
     >
-      <div className="h-1/2 border-b border-b-zinc-200">
+      <div className="h-1/2 p-1 border-b border-b-zinc-200">
         <Image
           src={prod.img[0]}
           alt="Alt de la imagen"
@@ -24,7 +24,7 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
           className="p-4 w-full"
         />
       </div>
-      <div>
+      <div className="md:p-1">
         <ProdPrice prod={prod} />
         <div className="flex justify-between md:gap-3 gap-1 items-center p-3">
           <h3 className="whitespace-normal">{prod.title.slice(0, 15)}...</h3>
@@ -39,7 +39,7 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
               }`}
           ></p>
         </div>
-        <p className="text-zinc-600 text-sm p-1 md:pb-10 pb-3 whitespace-normal">
+        <p className="text-zinc-600 text-sm p-2 md:pb-10 pb-3 whitespace-normal">
           {prod.description.slice(0, isMobile ? 20 : 80)}...
         </p>
       </div>
