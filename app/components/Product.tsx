16462db249bd68a7e67c35decadcc87ec2e89298
CamilaAznar <aznarcami@gmail.com>
2025-03-10 11:20:44 -0300

    changing mobile

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -13,7 +13,7 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
     <Link
       href={`/products/detail/${prod.id}`}
       key={index}
-      className="md:min-w-[250px] md:max-w-[250px] min-w-[150px] max-w-[150px] md:min-h-[450px] min-h-[200px] border border-zinc-200 bg-white rounded-md md:hover:shadow-lg transition-all duration-200 cursor-pointer"
+      className="md:min-w-[250px] md:max-w-[250px] min-w-[170px] max-w-[180px] md:min-h-[450px] min-h-[200px] border border-zinc-200 bg-white rounded-md md:hover:shadow-lg transition-all duration-200 cursor-pointer"
     >
       <div className="h-1/2 border-b border-b-zinc-200">
         <Image
@@ -26,8 +26,8 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
       </div>
       <div>
         <ProdPrice prod={prod} />
-        <div className="flex justify-between gap-3 items-center p-3">
-          <h3 className="whitespace-normal">{prod.title.slice(0, 15)}...</h3>
+        <div className="flex justify-between gap-1 items-center p-2">
+          <h3 className="whitespace-normal">{prod.title}</h3>
           <p
             className={`text-md bg-gradient-to-br text-zinc-800 w-fit px-2 py-2 rounded-full shadow-md 
               ${
@@ -39,8 +39,8 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
               }`}
           ></p>
         </div>
-        <p className="text-zinc-600 text-sm p-3 md:pb-10 pb-3 whitespace-normal">
-          {prod.description.slice(0, isMobile ? 40 : 80)}...
+        <p className="text-zinc-600 text-sm p-1 md:pb-10 pb-3 whitespace-normal">
+          {prod.description.slice(0, isMobile ? 20 : 80)}...
         </p>
       </div>
     </Link>
