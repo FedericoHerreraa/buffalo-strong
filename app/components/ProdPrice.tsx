58f09edd8dce9044f68826ee033ebec02d9d42a5
Federico Herrera <fede.juan.herrera@gmail.com>
2025-03-11 17:50:47 -0300

    updating price in cards

--- app/components/ProdPrice.tsx
+++ app/components/ProdPrice.tsx
@@ -16,7 +16,7 @@ export const ProdPrice = ({ prod } : { prod: ProductDB }) => {
                 </>
             ) : (
                 <>
-                    <p className="text-zinc-800">Precio comercio: <span className="font-semibold">${prod?.listPrice.toLocaleString('es-AR')}</span></p>
+                    <p className="text-zinc-800 flex md:gap-1 md:flex-row flex-col md:mb-0 mb-2">Precio comercio: <span className="font-semibold">${prod?.listPrice.toLocaleString('es-AR')}</span></p>
                     <p className="line-through text-sm text-zinc-700">${prod.sugestedPrice.toLocaleString('es-AR')}</p>
                 </>
             )}            
