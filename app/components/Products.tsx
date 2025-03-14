6c853f3bb10ab06c44326b01f11ddb8faa5f8f7e
Federico Herrera <fede.juan.herrera@gmail.com>
2025-03-14 20:19:58 -0300

    adding tabs to electricas

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -15,7 +15,7 @@ import { Spinner } from "../images/icons/Spinner";
 import { FiltersComponent } from "@/app/components/FiltersComponent";
 import logobuffalo from "@/app/images/logos/Logobuffalo.png";
 import { useMobileView } from "../context/MobileContext"
-
+import { Tabs, TabsList, TabsTrigger } from "@/app/components/ui/tabs"
 
 
 export const Products = () => {
@@ -49,7 +49,7 @@ export const Products = () => {
             </div>
         )
     }
-        
+
     return (
         <div id="products" className={`min-h-[100vh] ${open_sans.className} md:bg-gradient-to-b from-white via-zinc-200 to-white mt-20`}>
             <section className="flex gap-20 md:p-10 p-1 mb-5 md:w-[85%] w-[93%] mx-auto flex-wrap md:shadow-lg md:border-x bg-white md:border-x-zinc-200 md:rounded-b-lg ">
@@ -75,27 +75,37 @@ export const Products = () => {
                     const filteredProducts = prods?.filter(prod => prod.category === cat.keyValue).slice(0, 6);
                     return (
                         <div key={cat.keyValue} id={cat.keyValue} className={`mb-6 w-full ${filteredProducts.length === 0 ? 'bg-white' : 'bg-gradient-to-b from-white via-zinc-100 to-white'}`}>
-                            <div className="flex md:flex-row flex-col md:gap-5 gap-3 md:items-center mb-10">
+                            <div className="flex md:flex-row flex-col md:gap-5 gap-3 md:items-center mb-5">
                                 <h1
                                     className={`md:text-4xl text-3xl font-bold md:whitespace-nowrap bg-gradient-to-r from-amber-700 to-zinc-700 bg-clip-text text-transparent`}
-                                    >
+                                >
                                     {cat.title}
                                 </h1>
                                 <div className="flex items-center gap-2">
-                                    <Link 
+                                    <Link
                                         href={`/products/${cat.keyValue}`}
                                         className="w-fit whitespace-nowrap flex-shrink-0 min-w-max text-zinc-400 cursor-pointer hover:text-zinc-600 transition-all duration-150"
                                     >
-                                            Ver mas sobre esta categoria
+                                        Ver mas sobre esta categoria
                                     </Link>
-                                    <IoIosArrowRoundForward size={25}/>
+                                    <IoIosArrowRoundForward size={25} />
                                 </div>
                                 <div className="md:w-full w-0 md:h-[1px] h-0 bg-zinc-300"></div>
                             </div>
+                            {cat.keyValue === "Electricas" && (
+                                <Tabs defaultValue="Categoria 1" className="w-[400px] mb-5">
+                                    <TabsList className="border border-zinc-300 gap-1">
+                                        <TabsTrigger className="" value="">Categoria 1</TabsTrigger>
+                                        <TabsTrigger className="" value="">Categoria 1</TabsTrigger>
+                                        <TabsTrigger className="" value="">Categoria 1</TabsTrigger>
+                                        <TabsTrigger className="" value="">Categoria 1</TabsTrigger>
+                                    </TabsList>
+                                </Tabs>
+                            )}
                             <div className="flex md:gap-4 gap-1 overflow-x-auto whitespace-nowrap pb-5">
-                                {filteredProducts && filteredProducts.length > 0 ? ( 
+                                {filteredProducts && filteredProducts.length > 0 ? (
                                     filteredProducts.map((prod, index) => (
-                                        <Product key={index} prod={prod} index={index}/>
+                                        <Product key={index} prod={prod} index={index} />
                                     ))
                                 ) : (
                                     <p className="text-gray-700 text-xl font-semibold">No se encontraron productos en esta categoría.</p>
@@ -104,7 +114,7 @@ export const Products = () => {
                         </div>
                     );
                 })}
-            <StockReference />
+                <StockReference />
             </section>
         </div>
     )
