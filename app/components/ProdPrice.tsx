e980477f7689571a62f5b397d1964fb4a2556c8f
Federico Herrera <fede.juan.herrera@gmail.com>
2025-03-17 12:35:59 -0300

    improving productos

--- app/components/ProdPrice.tsx
+++ app/components/ProdPrice.tsx
@@ -8,7 +8,7 @@ export const ProdPrice = ({ prod } : { prod: ProductDB }) => {
     const { user } = useAuth()
 
     return (
-        <div className="text-zinc-800 p-3 flex flex-col">
+        <div className="text-zinc-800 p-1 flex flex-col">
             {!user ? (
                 <>
                     <p className="text-zinc-800 flex md:gap-1 md:flex-row flex-col md:mb-0 mb-2">Precio público: <span className="font-semibold">${prod?.sugestedPrice.toLocaleString('es-AR')}</span></p>
--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -16,7 +16,6 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
       className="flex flex-col border border-zinc-200 bg-white rounded-md md:hover:shadow-lg transition-all duration-200 cursor-pointer
                  w-full md:w-[250px] h-full md:h-[450px] max-w-[300px]"
     >
-      {/* Imagen del producto */}
       <div className="relative w-full h-[180px] md:h-[250px] overflow-hidden border-b border-b-zinc-200">
         <Image
           src={prod.img[0]}
@@ -26,8 +25,7 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
         />
       </div>
 
-      {/* Información del producto */}
-      <div className="flex flex-col flex-grow p-2 md:p-3">
+      <div className="flex flex-col flex-grow p-1 md:p-2">
         <ProdPrice prod={prod} />
 
         <div className="flex justify-between items-center py-2">
@@ -35,7 +33,7 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
             {prod.title.slice(0, 15)}...
           </h3>
           <p
-            className={`text-md bg-gradient-to-br text-zinc-800 w-fit px-2 py-1 rounded-full shadow-md 
+            className={`text-md bg-gradient-to-br text-zinc-800 w-fit px-2 py-2 rounded-full shadow-md 
               ${
                 prod.stock > 30
                   ? "from-green-300 to-green-600"
