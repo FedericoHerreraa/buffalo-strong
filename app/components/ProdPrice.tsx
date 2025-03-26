191863803d127f9033a58370ab8fb2762821c4d2
CamilaAznar <aznarcami@gmail.com>
2025-03-26 13:14:03 -0300

    punto6

--- app/components/ProdPrice.tsx
+++ app/components/ProdPrice.tsx
@@ -8,7 +8,7 @@ export const ProdPrice = ({ prod } : { prod: ProductDB }) => {
     const { user } = useAuth()
 
     return (
-        <div className="text-zinc-800 p-1 flex flex-col">
+        <div className="text-zinc-800 p-1 flex flex-col align-center ">
             {!user ? (
                 <>
                     <p className="text-zinc-800 flex md:gap-1 md:flex-row flex-col md:mb-0 mb-2">Precio público: <span className="font-semibold">${prod?.sugestedPrice.toLocaleString('es-AR')}</span></p>
@@ -16,8 +16,8 @@ export const ProdPrice = ({ prod } : { prod: ProductDB }) => {
             ) : (
                 <>
                     <p className="text-zinc-800 flex md:gap-1 md:flex-row flex-col md:mb-0 mb-2">Precio comercio: <span className="font-semibold">${prod?.listPrice.toLocaleString('es-AR')}</span></p>
-                    <p>Iva 21% | ${prod.listPrice * 0.21}</p>
-                    <p className="font-semibold">PVP: ${prod.sugestedPrice.toLocaleString('es-AR')}</p>
+                    <p className="text-zinc-700 flex md:mb-0 mb-2 text-sm ">Iva 21% | ${(prod.listPrice * 0.21).toFixed(2)}</p>
+                    <p className="text-zinc-800 flex md:mb-0 mb-2"><span className="font-semibold">PVP: </span> ${prod.sugestedPrice.toLocaleString('es-AR')}</p>
                 </>
             )}            
         </div>
--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -4,19 +4,33 @@ import { ProductDB } from "@/app/types/types";
 import Image from "next/image";
 import Link from "next/link";
 import { ProdPrice } from "./ProdPrice";
-import { useMobileView } from "@/app/context/MobileContext";
-// import { useAuth } from "@/app/context/AuthContext";
+import { useAuth } from "@/app/context/AuthContext";
+import { useState } from "react";
+import { useCart } from "../context/CartContext";
+import { redirect } from "next/navigation";
+import { MdAddShoppingCart } from "react-icons/md";
+
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/components/ui/select";
 
 export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => {
-  const { isMobile } = useMobileView();
-  // const { user } = useAuth()
+  // const { isMobile } = useMobileView();  
+  const [count, setCount] = useState<number>(1);
+  const { addToCart } = useCart()
+  const { user } = useAuth()
 
   return (
     <Link
       href={`/products/detail/${prod.id}`}
       key={index}
       className="flex flex-col border border-zinc-200 bg-white rounded-md md:hover:shadow-lg transition-all duration-200 cursor-pointer
-                 w-[150px] md:w-[250px] min-h-[300px] md:h-[450px]"
+                 w-[170px] md:w-[250px] min-h-[300px] md:h-[450px]"
     >
       <div className="relative w-full min-h-[150px] md:h-[250px] overflow-hidden border-b border-b-zinc-200">
         <Image
@@ -45,10 +59,36 @@ export const Product = ({ prod, index }: { prod: ProductDB, index: number }) => 
               }`}
           ></p>
         </div>
-
+{/* 
         <p className="text-zinc-600 text-xs md:text-sm whitespace-normal">
           {prod.description.slice(0, isMobile ? 20 : 80)}...
-        </p>
+        </p> */}
+        <div className="flex gap-1 w-[200px] mt-10">
+                <Select value={count.toString()} onValueChange={(value) => setCount(Number(value))}>
+                    <SelectTrigger className="p-2 gap-3 border-zinc-300 text-zinc-500 w-[100px] md:w-[200px]">
+                        <SelectValue placeholder="Seleccionar cantidad" />
+                    </SelectTrigger>
+                    <SelectContent className="p-2 border-zinc-300 bg-white text-zinc-600">
+                        {Array.from({ length: prod.stock }, (_, i) => i + 1).map((qty) => (
+                            <SelectItem key={qty} value={qty.toString()} className="border-none cursor-pointer hover:bg-zinc-500">
+                                {qty}
+                            </SelectItem>
+                        ))}
+                    </SelectContent>
+                </Select>
+                <button
+                    disabled={count === 0}
+                    onClick={() => {
+                        if (!user) redirect('/register')
+
+                        addToCart({ ...prod, quantity: count })
+                        setCount(1)
+                    }}
+                    className="bg-zinc-800 px-4 py-2 rounded-lg text-zinc-300 hover:bg-zinc-600 hover:text-white transition-all duration-150"
+                >
+                    <MdAddShoppingCart />
+                </button>
+            </div>
       </div>
     </Link>
   );
